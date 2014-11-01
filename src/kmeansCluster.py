62f85891d47745c875d312d48c85889b2e1535c5
Nick Robinson <nr33@msstate.edu>
2014-11-01 18:14:59 -0500

    Updating initial commit with PCA changes and command line arguments

--- src/kmeansCluster.py
+++ src/kmeansCluster.py
@@ -3,80 +3,81 @@ print(__doc__)
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pdb
+import plotFunctions
 
 from sklearn.cluster import KMeans
-from sklearn import datasets
+from sklearn.utils import shuffle
+from sklearn import datasets, metrics
+from sklearn.decomposition import PCA
+
+from optparse import OptionParser
 
 from fileHelper import Wines
 
 ###############################################################################
+#Command Line Parsing
+# parse commandline arguments
+op = OptionParser()
+op.add_option("--clusters",
+              dest="num_clusters", type="int",
+              help="Number of clusters to use for KMeans.")
+
+op.add_option("--minibatch",
+              action="store_true", dest="minibatch", default=False,
+              help="Use batch mode k-means algorithm.")
+
+op.add_option("--max_iter",
+              dest="max_iter", type="int", default=300,
+              help="Number of maximum iterations for KMeans.")
+
+op.add_option("--analysis",
+              dest="analysis", type="str", default="",
+              help="Analysis type to use (PCA, ICA).")
+
+op.add_option("--n_components",
+              dest="n_components", type="int", default=2,
+              help="Number of component attributes to be used with ICA, PCA algorithms.")
+
+print(__doc__)
+op.print_help()
+
+(opts, args) = op.parse_args()
+if len(args) > 0:
+    op.error("this script takes no arguments.")
+    sys.exit(1)
+
+
+###############################################################################
 # Load data
-datafile = '../data/winequality-white.csv'
+datafile = '../data/winequality-red.csv'
 wines = Wines()
 wines.loadData(datafile)
 X, y = shuffle(wines.data, wines.target, random_state=5)
 X = X.astype(np.float64)
 y = y.astype(np.float64)
-offset = int(X.shape[0] * 0.80)
+offset = int(X.shape[0] * 0.20)
 X_train, y_train = X[:offset], y[:offset]
 X_test, y_test = X[offset:], y[offset:]
 
-np.random.seed(5)
+if opts.analysis:
+  if opts.analysis == "PCA":
+    # in this case the seeding of the centers is deterministic, hence we run the
+    # kmeans algorithm only once with n_init=1
+    pca = PCA(n_components=opts.n_components)
+    reduced_data = pca.fit_transform(X_train)
+    km = KMeans(n_clusters=opts.num_clusters, max_iter=opts.max_iter)
+    km.fit(reduced_data)
+else:
+    km = KMeans(n_clusters=opts.num_clusters, max_iter=opts.max_iter)
+    km.fit(pca)
 
-centers = [[1, 1], [-1, -1], [1, -1]]
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+labels = km.labels_
 
-estimators = {'k_means_iris_3': KMeans(n_clusters=3),
-              'k_means_iris_8': KMeans(n_clusters=8),
-              'k_means_iris_bad_init': KMeans(n_clusters=3, n_init=1,
-                                              init='random')}
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(y_train, km.labels_))
+print("Completeness: %0.3f" % metrics.completeness_score(y_train, km.labels_))
+print("V-measure: %0.3f" % metrics.v_measure_score(y_train, km.labels_))
 
-
-fignum = 1
-for name, est in estimators.items():
-    fig = plt.figure(fignum, figsize=(4, 3))
-    plt.clf()
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
-    plt.cla()
-    est.fit(X)
-    labels = est.labels_
-
-    ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=labels.astype(np.float))
-
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_xlabel('Petal width')
-    ax.set_ylabel('Sepal length')
-    ax.set_zlabel('Petal length')
-    fignum = fignum + 1
-
-# Plot the ground truth
-fig = plt.figure(fignum, figsize=(4, 3))
-plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
-plt.cla()
-
-for name, label in [('Setosa', 0),
-                    ('Versicolour', 1),
-                    ('Virginica', 2)]:
-    ax.text3D(X[y == label, 3].mean(),
-              X[y == label, 0].mean() + 1.5,
-              X[y == label, 2].mean(), name,
-              horizontalalignment='center',
-              bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-# Reorder the labels to have colors matching the cluster results
-y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y)
-
-ax.w_xaxis.set_ticklabels([])
-ax.w_yaxis.set_ticklabels([])
-ax.w_zaxis.set_ticklabels([])
-ax.set_xlabel('Petal width')
-ax.set_ylabel('Sepal length')
-ax.set_zlabel('Petal length')
-plt.show()
+if opts.analysis:
+  if opts.analysis == "PCA":
+    plotFunctions.plotPCAData(reduced_data, km)
