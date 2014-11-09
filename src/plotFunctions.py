1c8ba72b5605aa0eecb33cdc4849c1ea6445dd1e
Nick Robinson <nr33@msstate.edu>
2014-11-09 17:14:47 -0600

    Uploading newest version of notebook with Iris data

--- src/plotFunctions.py
+++ src/plotFunctions.py
@@ -2,6 +2,7 @@ import numpy as np
 import itertools
 from scipy.spatial.distance import cdist, pdist
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pdb
 
 from sklearn.cluster import KMeans
@@ -131,3 +132,83 @@ def scatterplot_matrix(data, names=[], **kwargs):
         axes[-1,-1].set_ylim(ylimits)
 
     return fig
+
+def scatterplot_em(clf, data, names=[], **kwargs):
+    """
+    Plots a scatterplot matrix of subplots.  Each row of "data" is plotted
+    against other rows, resulting in a nrows by nrows grid of subplots with the
+    diagonal subplots labeled with "names".  Additional keyword arguments are
+    passed on to matplotlib's "plot" command. Returns the matplotlib figure
+    object containg the subplot grid.
+    """
+    numvars, numdata = data.shape
+    fig, axes = plt.subplots(nrows=numvars, ncols=numvars, figsize=(30,30))
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+
+    pdb.set_trace()
+
+    for ax in axes.flat:
+        # Hide all ticks and labels
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+        # Set up ticks only on one side for the "edge" subplots...
+        if ax.is_first_col():
+            ax.yaxis.set_ticks_position('left')
+        if ax.is_last_col():
+            ax.yaxis.set_ticks_position('right')
+        if ax.is_first_row():
+            ax.xaxis.set_ticks_position('top')
+        if ax.is_last_row():
+            ax.xaxis.set_ticks_position('bottom')
+
+    # Plot the data.
+    for i, j in zip(*np.triu_indices_from(axes, k=1)):
+        for x, y in [(i,j), (j,i)]:
+            # FIX #1: this needed to be changed from ...(data[x], data[y],...)
+            x = np.linspace(i[:, 0].min(), i[:, 0].max())
+            y = np.linspace(j[:, 1].min(), j[:, 1].max())
+            X, Y = np.meshgrid(x, y)
+            XX = np.array([X.ravel(), Y.ravel()]).T
+            Z = -clf.score_samples(XX)[0]
+            Z = Z.reshape(X.shape)
+            CS = axes[x,y].contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
+                 levels=np.logspace(0, 3, 10))
+            CB = axes[x,y].colorbar(CS, shrink=0.8, extend='both')
+            axes[x,y].scatter(X_train[:, 0], X_train[:, 1], .8)
+
+    # Label the diagonal subplots...
+    if not names:
+        names = ['x'+str(i) for i in range(numvars)]
+
+    for i, label in enumerate(names):
+        axes[i,i].annotate(label, (0.5, 0.5), xycoords='axes fraction',
+                ha='center', va='center')
+
+    # Turn on the proper x or y axes ticks.
+    for i, j in zip(range(numvars), itertools.cycle((-1, 0))):
+        axes[j,i].xaxis.set_visible(True)
+        axes[i,j].yaxis.set_visible(True)
+
+    # FIX #2: if numvars is odd, the bottom right corner plot doesn't have the
+    # correct axes limits, so we pull them from other axes
+    if numvars%2:
+        xlimits = axes[0,-1].get_xlim()
+        ylimits = axes[-1,0].get_ylim()
+        axes[-1,-1].set_xlim(xlimits)
+        axes[-1,-1].set_ylim(ylimits)
+
+    return fig
+
+def make_ellipses(gmm, ax):
+    for n, color in enumerate('rgb'):
+        v, w = np.linalg.eigh(gmm._get_covars()[n][:2, :2])
+        u = w[0] / np.linalg.norm(w[0])
+        angle = np.arctan2(u[1], u[0])
+        angle = 180 * angle / np.pi  # convert to degrees
+        v *= 9
+        ell = mpl.patches.Ellipse(gmm.means_[n, :2], v[0], v[1],
+                                  180 + angle, color=color)
+        ell.set_clip_box(ax.bbox)
+        ell.set_alpha(0.5)
+        ax.add_artist(ell)
