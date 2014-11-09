30407b807f4d66ca2fcd745059b4a2b1f27a72cc
Nick Robinson <nr33@msstate.edu>
2014-11-09 09:56:48 -0600

    Updating notebook with ICA and RP

--- src/plotFunctions.py
+++ src/plotFunctions.py
@@ -2,6 +2,7 @@ import numpy as np
 import itertools
 from scipy.spatial.distance import cdist, pdist
 import matplotlib.pyplot as plt
+import pdb
 
 from sklearn.cluster import KMeans
 
@@ -10,8 +11,8 @@ def plotData(reduced_data, km, algorithm):
   h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
 
   # Plot the decision boundary. For that, we will assign a color to each
-  x_min, x_max = reduced_data[:, 0].min() + 1, reduced_data[:, 0].max() - 1
-  y_min, y_max = reduced_data[:, 1].min() + 1, reduced_data[:, 1].max() - 1
+  x_min, x_max = reduced_data[:, 0].min(), reduced_data[:, 0].max()
+  y_min, y_max = reduced_data[:, 1].min(), reduced_data[:, 1].max()
   xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
   # Obtain labels for each point in mesh. Use last trained model.
@@ -22,18 +23,18 @@ def plotData(reduced_data, km, algorithm):
   plt.figure(1)
   plt.clf()
   plt.imshow(Z, interpolation='nearest',
-             extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-             cmap=plt.cm.Paired,
-             aspect='auto', origin='lower')
+            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+            cmap=plt.cm.Paired,
+            aspect='auto', origin='lower')
 
   plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
   # Plot the centroids as a white X
   centroids = km.cluster_centers_
   plt.scatter(centroids[:, 0], centroids[:, 1],
               marker='x', s=169, linewidths=3,
-              color='w', zorder=10)
+              color='r', zorder=10)
   plt.title('K-means clustering on the digits dataset (' + algorithm + '-reduced data)\n'
-            'Centroids are marked with white cross')
+            'Centroids are marked with red cross')
   plt.xlim(x_min, x_max)
   plt.ylim(y_min, y_max)
   plt.xticks(())
